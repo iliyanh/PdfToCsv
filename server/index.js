dbddf7fbb6bd544b52a6d0f0119cf7ca3411c198
Iliyan Hadzhiev <122830980+iliyanh@users.noreply.github.com>
2025-08-02 17:31:41 +0100

    Setup Cors and body-parser

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
